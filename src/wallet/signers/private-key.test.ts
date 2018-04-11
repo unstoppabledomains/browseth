f9ada4742cfab7b4cda2559946c10edd9db3e89f
Tim Jose <timjose3@gmail.com>
2018-04-10 17:11:18 -0700

    added keystore :) 

--- src/wallet/signers/private-key.test.ts
+++ src/wallet/signers/private-key.test.ts
@@ -9,14 +9,59 @@ const tests = [
     'warfare divide diesel hover breeze congress phone rabbit custom lizard high dash',
     '7ede8658489295b952dda02d70df9baa2a3b577e10f456dae598aba867f48734d79afa9ff4bd241aafe253c1631bb6f1e5a641c1093fb3330b353a8235071885',
   ],
+  [
+    'trial day photo kidney spend eagle frown senior volcano remain lake govern',
+    '5521d29a63a4e2f9c423f76d796eff59937443e9ca41c8a22991ff7acdc5ed005c84e7dd7b73a9a23f62315ddfd5e1aa8707d99d2a1a4e56b54b22aa5f03e156',
+    'hello',
+  ],
+  [
+    'agent certain cricket club season net arch sample runway river omit will network fashion bird',
+    'd21eb7838d9e73b5c2d7418631ab4139c34f3061d19de70e1c48d9ed8f2a642e46c984e56087e794b35972e65b4fad3cd376c17e063189ef0ce4c768444dd5c4',
+  ],
 ];
 
 describe('mnemonic tests', () => {
-  tests.forEach(([input, output]) => {
+  tests.forEach(([input, output, pw]) => {
     test(`${input} => ${output}`, () => {
       expect(
-        PrivateKey.fromMnemonic(input.split(' ')).privateKey.toString('hex'),
+        PrivateKey.fromMnemonic(input.split(' '), pw).privateKey.toString(
+          'hex',
+        ),
       ).toBe(output);
     });
   });
+});
+
+const obj1 = {
+  version: 3,
+  id: 'b919560d-346b-44d4-92a9-8059c6ee7989',
+  address: '82137e3b5a4fd84250bbfbad8c58e65a4460f991',
+  Crypto: {
+    ciphertext:
+      'f599218f7155512595ee5f191ae922f2c61ba6560c1ac283a1162569f3f490ba',
+    cipherparams: {iv: '629773b80c288af597383d029ce4a24d'},
+    cipher: 'aes-128-ctr',
+    kdf: 'scrypt',
+    kdfparams: {
+      dklen: 32,
+      salt: 'a41900b88518ffd14ee04bbac893d705e4231d52580c9bc72f3c277e663892cd',
+      n: 8192,
+      r: 8,
+      p: 1,
+    },
+    mac: '2dd028de90423af0c47549afa819c52a99bd36df2cbf11ad3b386aec79a203ba',
+  },
+};
+
+// const pk = PrivateKey.fromV3(obj1, '111111111');
+// test('simple test for fromV3()', () => {
+//   expect(pk.privateKey.toString('hex')).toBe(
+//     'ed0137b4b079d340b28290ec11972cf9c6983e83f04fee2351e3709b464f1b2f',
+//   );
+// });
+test('generate new private key, make a keystore from it with toV3() then compare the private keys with toV3()', async () => {
+  const newPk = PrivateKey.fromRandomBytes();
+  const keystore = await newPk.toV3('password', {});
+  const check = PrivateKey.fromV3(keystore, 'password');
+  expect(check.toString()).toBe(newPk.toString());
 });
--- src/wallet/signers/private-key.ts
+++ src/wallet/signers/private-key.ts
@@ -1,32 +1,97 @@
+import {
+  createCipheriv,
+  createDecipheriv,
+  createHmac,
+  Hash,
+  randomBytes,
+} from 'crypto';
 import EthereumJsTx from 'ethereumjs-tx';
 import {secp256k1} from 'ethereumjs-util';
+import {privateToAddress, sha3} from 'ethereumjs-util';
 import createKeccak from 'keccak';
 import {pbkdf2Sync} from 'pbkdf2';
-// import secp256k1 from 'secp256k1';
+import * as scryptsy from 'scryptsy';
+import * as uuidv4 from 'uuid/v4';
 import {Signer} from './types';
+
+export interface KdfParams {
+  dklen: number;
+  n?: number;
+  c?: number;
+  r?: number;
+  p?: number;
+  salt: string;
+  prf?: string;
+}
 
 export class PrivateKey implements Signer {
   public static fromV1(json: any, passphrase: string): PrivateKey {
     return new PrivateKey(undefined!);
   }
-  public static fromV3(json: any, passphrase: string): PrivateKey {
-    return new PrivateKey(Buffer.from(''));
+  public static fromV3(keyStore: any, pw: string) {
+    keyStore = JSON.parse(keyStore.toLowerCase());
+    if (keyStore.version !== 3) {
+      throw new Error('Not a V3 wallet');
+    }
+    let derivedKey;
+    const kdfparams = keyStore.crypto.kdfparams;
+    if (keyStore.crypto.kdf === 'pbkdf2') {
+      derivedKey = pbkdf2Sync(
+        Buffer.from(pw),
+        Buffer.from(kdfparams.salt, 'hex'), // salt
+        kdfparams.c || 262144, // iterations
+        kdfparams.dklen, // length
+        'sha256',
+      );
+    } else if (keyStore.crypto.kdf === 'scrypt') {
+      derivedKey = scryptsy(
+        Buffer.from(pw),
+        Buffer.from(kdfparams.salt, 'hex'),
+        kdfparams.n || 262144,
+        kdfparams.r, // memory factor
+        kdfparams.p, // parallelization factor
+        kdfparams.dklen,
+      );
+    } else {
+      throw new Error('Unsupported kdf');
+    }
+    const ciphertext = Buffer.from(keyStore.crypto.ciphertext, 'hex');
+    const mac = sha3(Buffer.concat([derivedKey.slice(16, 32), ciphertext]));
+
+    if (mac.toString('hex') !== keyStore.crypto.mac) {
+      throw new Error('Macs do not match. Check password');
+    }
+    const decipher = createDecipheriv(
+      keyStore.crypto.cipher,
+      derivedKey.slice(0, 16),
+      Buffer.from(keyStore.crypto.cipherparams.iv, 'hex'),
+    );
+    const seed = Buffer.concat([decipher.update(ciphertext), decipher.final()]);
+    return new PrivateKey(seed);
   }
 
   public static fromHex(raw: string): PrivateKey {
     return new PrivateKey(Buffer.from(raw.replace('0x', ''), 'hex'));
   }
 
-  public static fromMnemonic(phrase: string | string[]): PrivateKey {
+  // returns bip39 seed
+  public static fromMnemonic(
+    phrase: string | string[],
+    password?: string,
+  ): PrivateKey {
     return new PrivateKey(
       pbkdf2Sync(
         typeof phrase === 'string' ? phrase : phrase.join(' '),
-        'mnemonic',
+        password ? `mnemonic${password}` : 'mnemonic',
         2048,
         64,
         'SHA512',
       ),
     );
+  }
+
+  public static fromRandomBytes() {
+    return new PrivateKey(randomBytes(32));
   }
 
   public static privateToAddress(privateKey: Buffer) {
@@ -45,10 +110,6 @@ export class PrivateKey implements Signer {
   }
 
   constructor(public privateKey: Buffer) {}
-
-  public toMnemonic() {
-    //
-  } // one string with spaces
 
   public account = () =>
     Promise.resolve(PrivateKey.privateToAddress(this.privateKey));
@@ -76,4 +137,85 @@ export class PrivateKey implements Signer {
         ]).toString('hex'),
     );
   };
+  // returns keystore object and private key (randomly generated if not provided)
+  public toV3(
+    pw: string,
+    {
+      salt = randomBytes(32),
+      iv = randomBytes(16),
+      uuid = randomBytes(16),
+      kdf = 'scrypt',
+      cipher = 'aes-128-ctr',
+      dklen = 32,
+      c = 262144,
+      n = 262144,
+      r = 8,
+      p = 1,
+    } = {},
+  ): Promise<any> {
+    return new Promise(resolve => {
+      const kdfparams: KdfParams = {
+        dklen,
+        salt: salt.toString('hex'),
+      };
+      let derivedKey;
+
+      if (kdf === 'pbkdf2') {
+        kdfparams.c = c;
+        kdfparams.prf = 'hmac-sha256';
+        derivedKey = pbkdf2Sync(
+          Buffer.from(pw),
+          salt,
+          kdfparams.c,
+          kdfparams.dklen,
+          'sha256',
+        );
+      } else if (kdf === 'scrypt') {
+        kdfparams.n = n;
+        kdfparams.r = r;
+        kdfparams.p = p;
+        derivedKey = scryptsy(
+          Buffer.from(pw),
+          salt,
+          kdfparams.n,
+          kdfparams.r,
+          kdfparams.p,
+          kdfparams.dklen,
+        );
+      } else {
+        throw new Error('Unsupported kdf');
+      }
+      const ciph = createCipheriv(cipher, derivedKey.slice(0, 16), iv);
+      if (!ciph) {
+        throw new Error('Unsupported cipher');
+      }
+      const ciphertext = Buffer.concat([
+        ciph.update(this.privateKey),
+        ciph.final(),
+      ]);
+      const mac = sha3(Buffer.concat([derivedKey.slice(16, 32), ciphertext]));
+
+      resolve(
+        JSON.stringify({
+          version: 3,
+          id: uuidv4({random: uuid}),
+          address: privateToAddress(this.privateKey).toString('hex'),
+          crypto: {
+            ciphertext: ciphertext.toString('hex'),
+            cipherparams: {
+              iv: iv.toString('hex'),
+            },
+            cipher,
+            kdf,
+            kdfparams,
+            mac: mac.toString('hex'),
+          },
+        }),
+      );
+    });
+  }
+
+  public toString() {
+    return this.privateKey.toString('hex');
+  }
 }
